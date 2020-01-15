c187d557f6bb85581b4e99ba2b3fbbc519b8bd3e
Tom Patton <tom.patton22@gmail.com>
2020-01-14 23:17:59 -0800

    [REACT-CONCEPTS] Lesson 4: Stateful and stateless components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Alert from './ui-components/Alert';
 
-function App() {
-  return (
-  <>
-    <div className="container">
-      <Alert type="success">
-        <span>This is a small alert message</span>
-      </Alert>
-    </div>
-    <div className="container">
-      <Alert
-        message="Hello World"
-        type="danger"
-      />
-    </div>
-    <div className="container">
-      <Alert
-        message="Hello World"
-        type="primary"
-      />
-    </div>
-  </>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showAlert: true
+    };
+  }
+
+  render() {
+    return (
+    <>
+      <div className="container">
+        <Alert type="success">
+          <span>This is a small alert message</span>
+        </Alert>
+      </div>
+      <div className="container">
+        <Alert
+          message="Hello World"
+          type="danger"
+        />
+      </div>
+      <div className="container">
+        <Alert
+          message="Hello World"
+          type="primary"
+        />
+      </div>
+    </>
+    );
+  }
 }
 
 export default App;
--- src/ui-components/Alert.jsx
+++ src/ui-components/Alert.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Alert  = ({message, type, children}) => {
+const Alert = ({ message, type, children, show }) => {
   return (
-    <div className={`alert alert-${type}`} role="alert">
-      { message ? message : children }
-    </div>
+    <>
+      {show && (
+        <div className={`alert alert-${type}`} role="alert">
+          {message ? message : children}
+        </div>
+      )}
+    </>
   );
 };
 
 Alert.propTypes = {
   type: PropTypes.string.isRequired,
-  message: PropTypes.string
+  message: PropTypes.string,
+  show: PropTypes.bool
 }
 
 Alert.defaultProps = {
-  message: null
+  message: null,
+  show: true
 }
 
 export default Alert;
