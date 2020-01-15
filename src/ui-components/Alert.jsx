8ff83c49bc9f03fde9de35a6163d521187911dfd
Tom Patton <tom.patton22@gmail.com>
2020-01-14 23:02:11 -0800

    [REACT-CONCEPTS] Lesson 3: Component prop types

--- src/ui-components/Alert.jsx
+++ src/ui-components/Alert.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Alert  = ({message, type, children}) => {
   return (
-    <div class={`alert alert-${type}`} role="alert">
+    <div className={`alert alert-${type}`} role="alert">
       { message ? message : children }
     </div>
   );
 };
+
+Alert.propTypes = {
+  type: PropTypes.string.isRequired,
+  message: PropTypes.string
+}
+
+Alert.defaultProps = {
+  message: null
+}
 
 export default Alert;
